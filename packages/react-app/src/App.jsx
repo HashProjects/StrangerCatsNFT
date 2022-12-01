c9ca1948750f07500f5bccf57a7c84bff2d68d32
dannytzoc <tzocdanny@gmail.com>
2022-11-30 19:07:15 -0800

    About Changes

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -726,6 +726,16 @@ function App() {
               Debug Contracts
             </Link>
           </Menu.Item>
+          <Menu.Item key="/About">
+            <Link
+              onClick={() => {
+                setRoute("/About");
+              }}
+              to="/About"
+            >
+              About the Team
+            </Link>
+          </Menu.Item>
         </Menu>
         <Switch>
           <Route exact path="/">
@@ -897,6 +907,65 @@ function App() {
               blockExplorer={blockExplorer}
               contractConfig={contractConfig}
             />
+          </Route>
+          <Route path="/About">
+<h2 style={{ padding: 16, width: 500, margin: "auto", paddingBottom: 16 }}>Our Team</h2>
+<div className="row">
+  <div className="column">
+    <div class="card">
+    <img src="https://placekitten.com/200/300" alt="Jane" style={{width: 100}}/>
+      <div className="container">
+        <h2>Eric Britten</h2>
+        <p className="title">CEO & Founder</p>
+        <p>Favorite Cats are Siamese cat</p>
+        <p>jane@example.com</p>
+        <p><button className="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+
+  <div className="column">
+    <div className="card">
+      
+      <div className="container">
+      <img src="https://placekitten.com/201/300" alt="Jane" style={{width: 100}}/>
+        <h2>Rosa Cho</h2>
+        <p className="title">Programer</p>
+        <p>Favorite Cats are Persian Cat</p>
+        <p>mike@example.com</p>
+        <p><button className="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div className="column">
+    <div className="card">
+      
+      <div className="container">
+      <img src="https://placekitten.com/202/300" alt="Jane" style={{width: 100}}/>
+        <h2>Timothy Jan</h2>
+        <p className="title">Programer</p>
+        <p>Favorite Cats are Scottish Fold</p>
+        <p>john@example.com</p>
+        <p><button className="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+  <div className="column">
+    <div className="card">
+      
+      <div className="container">
+      <img src="https://placekitten.com/203/300" alt="Jane" style={{width: 100}}/>
+        <h2>Danny Tzoc</h2>
+        <p className="title">Designer</p>
+        <p>Favorite Cats are Sphynx cat</p>
+        <p>mike@example.com</p>
+        <p><button className="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+</div>
           </Route>
         </Switch>
       </BrowserRouter>
